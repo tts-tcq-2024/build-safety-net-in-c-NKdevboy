f97955cef662a4002880a12ee9232ec824cc9d7d
NKdevboy <nithishkumarramup@gmail.com>
2024-07-09 19:21:07 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,16 +1,20 @@
 #include <gtest/gtest.h>
 #include "Soundex.h"
 
-/*
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) 
 {
   char soundex[5];
   generateSoundex("AX", soundex);
 }
-*/
+
 TEST(SoudexTestsuite1, ReplacesConsonantsWithAppropriateDigits1) 
 {
   char soundex[5];
   generateSoundex("Aeiou", soundex);
 }
 
+TEST(SoudexTestsuite2, ReplacesConsonantsWithAppropriateDigits2) 
+{
+  char soundex[5];
+  generateSoundex("BCDM", soundex);
+}
