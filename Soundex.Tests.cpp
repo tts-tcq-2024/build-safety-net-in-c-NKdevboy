4e8e5f9e588f3ec800fee82684527b6426cfb974
Nithishkumar R (MS/ESG1-VM) <R.Nithishkumar@in.bosch.com>
2024-08-04 14:17:50 +0530

    Typo fix

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -5,21 +5,21 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits)
 {
   char soundex[5];
   generateSoundex("AX", soundex);
-  EXPECT_STREQ(soundex,A200);
+  EXPECT_STREQ(soundex,"A200");
 }
 
 TEST(SoudexTestsuite1, ReplacesConsonantsWithAppropriateDigits1) 
 {
   char soundex[5];
   generateSoundex("Aeiou", soundex);
-  EXPECT_STREQ(soundex,A000);
+  EXPECT_STREQ(soundex,"A000");
 }
 
 TEST(SoudexTestsuite2, ReplacesConsonantsWithAppropriateDigits2) 
 {
   char soundex[5];
   generateSoundex("BCDM", soundex);
-  EXPECT_STREQ(soundex,B235);
+  EXPECT_STREQ(soundex,"B235");
 }
 
 TEST(SoudexTestsuite3, ReplacesConsonantsWithAppropriateDigits3) 
